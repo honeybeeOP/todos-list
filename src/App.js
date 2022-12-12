989352a9eb43966cddab840e2e4b2fa2609a723c
honeybeeOP <neerajanchaudhary@gmail.com>
2022-12-12 17:48:36 +0545

    some important changes

--- src/App.js
+++ src/App.js
@@ -1,86 +1,78 @@
 import './App.css';
 import Header from "./MyComponents/Header";
-import {Todos} from "./MyComponents/Todos";
-import {Footer} from "./MyComponents/Footer";
-import {AddTodo} from "./MyComponents/AddTodo";
+import {Footer} from "./MyComponents/Footer"
 import {About} from "./MyComponents/About";
+import { Main } from './MyComponents/Main';
 
 
 import React,{useState, useEffect} from 'react';
 import{
-  BrowserRouter as Router,
+  BrowserRouter,
   Route,
   Routes,
 }from "react-router-dom";
 
 function App() {
-  let initTodo;
-  if(localStorage.getItem("todos")===null){
-    initTodo = [];
-  }
-  else{
-    initTodo = JSON.parse(localStorage.getItem("todos"));
-  }
+//   let initTodo;
+//   if(localStorage.getItem("todos")===null){
+//     initTodo = [];
+//   }
+//   else{
+//     initTodo = JSON.parse(localStorage.getItem("todos"));
+//   }
 
 
 
-  const onDelete = (todo)=>{
-    console.log("i am ondelete of todo", todo)
-    //deleting this was in react does not work
-    // let index = todos.indexOf(todo);
-    // todos.splice(index, 1);
+//   const onDelete = (todo)=>{
+//     console.log("i am ondelete of todo", todo)
+//     //deleting this was in react does not work
+//     // let index = todos.indexOf(todo);
+//     // todos.splice(index, 1);
 
-    setTodos(todos.filter((e)=>{
-      return e!==todo;
-    }));
-    console.log("deleted", todos)
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }
+//     setTodos(todos.filter((e)=>{
+//       return e!==todo;
+//     }));
+//     console.log("deleted", todos)
+//     localStorage.setItem("todos", JSON.stringify(todos));
+//   }
 
-  const addTodo = (title, desc)=>{
-    console.log("i am adding this todo",title,desc)
-    let sno;
-    if(todos.length === 0){
-      sno = 0;
-    }
-    else{
-      sno = todos[todos.length-1].sno+1;
-    }
+//   const addTodo = (title, desc)=>{
+//     console.log("i am adding this todo",title,desc)
+//     let sno;
+//     if(todos.length === 0){
+//       sno = 0;
+//     }
+//     else{
+//       sno = todos[todos.length-1].sno+1;
+//     }
     
-    const myTodo = {
-      sno: sno,
-      title: title,
-      desc: desc,
-    }
-    setTodos([...todos, myTodo]);
-    console.log(myTodo)  
- }
+//     const myTodo = {
+//       sno: sno,
+//       title: title,
+//       desc: desc,
+//     }
+//     setTodos([...todos, myTodo]);
+//     console.log(myTodo)  
+//  }
 
-  const [todos, setTodos] = useState(initTodo);
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos])  
+//   const [todos, setTodos] = useState(initTodo);
+//   useEffect(() => {
+//     localStorage.setItem("todos", JSON.stringify(todos));
+//   }, [todos])  
 
   return (
-    <>
-    <Router>
-      <Header title ="My Todos List" searchBar={false}/>
-      <Routes>
-        <Route exact path="/" render={()=>{
-          return
-          <>
-          <AddTodo addTodo={addTodo}/>
-          <Todos todos={todos} onDelete={onDelete}/>  
-          </>
-          }}>
-        </Route>
-        <Route exact path="/about" element={<About/>}/> 
+  
+    <BrowserRouter>
+    <Routes>
+      {/* <Header title ="My Todos List" searchBar={false}/> */}
+      <Route path="/" element={<Main />} />
+      <Route path="/" element={<Header />} />
+      <Route path="/about" element={<About />} />
+    
       </Routes>
-
-
       <Footer/>
-    </Router>
-    </>
+    </BrowserRouter>
+  
   );
 }
 
---
+++ src/MyComponents/Main.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import {Todos} from "./Todos";
+import {AddTodo} from "./AddTodo";
+import { useState, useEffect } from 'react';
+
+export const Main = () => {
+    let initTodo;
+    if(localStorage.getItem("todos")===null){
+      initTodo = [];
+    }
+    else{
+      initTodo = JSON.parse(localStorage.getItem("todos"));
+    }
+  
+    const onDelete = (todo)=>{
+      console.log("i am ondelete of todo", todo)
+      //deleting this was in react does not work
+      // let index = todos.indexOf(todo);
+      // todos.splice(index, 1);
+  
+      setTodos(todos.filter((e)=>{
+        return e!==todo;
+      }));
+      console.log("deleted", todos)
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }
+  
+    const addTodo = (title, desc)=>{
+      console.log("i am adding this todo",title,desc)
+      let sno;
+      if(todos.length === 0){
+        sno = 0;
+      }
+      else{
+        sno = todos[todos.length-1].sno+1;
+      }
+      
+      const myTodo = {
+        sno: sno,
+        title: title,
+        desc: desc,
+      }
+      setTodos([...todos, myTodo]);
+      console.log(myTodo)  
+   }
+  
+    const [todos, setTodos] = useState(initTodo);
+    useEffect(() => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos])  
+  return (
+    <div>       
+        <AddTodo addTodo={addTodo} />
+        <Todos todos={todos} onDelete={onDelete} />
+    </div>
+  )
+}
